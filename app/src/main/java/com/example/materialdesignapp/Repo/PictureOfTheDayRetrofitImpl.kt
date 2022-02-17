0c71b6a43c838dd6fe63501568cf45ff0f7777a8
docAlexxx <docalex@mail.ru>
2022-02-17 22:35:20 +0400

    lesson1: added Repository with retrofit

---
+++ app/src/main/java/com/example/materialdesignapp/Repo/PictureOfTheDayRetrofitImpl.kt
@@ -0,0 +1,18 @@
+package com.example.materialdesignapp.Repo
+
+import com.google.gson.GsonBuilder
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class PictureOfTheDayRetrofitImpl {
+
+    private val baseUrl = "https://api.nasa.gov/"
+
+    fun getRetrofitImpl():PictureOfTheDayAPI{
+        val podRetrofitImpl = Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(GsonConverterFactory.create(GsonBuilder().setLenient().create()))
+            .build()
+        return podRetrofitImpl.create(PictureOfTheDayAPI::class.java)
+    }
+}
