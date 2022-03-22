d5fbb4ded422fb9e52ecec28afe9efaa71b1cba3
docAlexxx <docalex@mail.ru>
2022-03-22 18:33:22 +0400

    lesson3: added custom view for TabLayout

--- app/src/main/java/com/example/materialdesignapp/view/viewpager/ViewPagerAdapter.kt
+++ app/src/main/java/com/example/materialdesignapp/view/viewpager/ViewPagerAdapter.kt
@@ -4,8 +4,8 @@ import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentStatePagerAdapter
 
 const val EARTH = 0
-const val MARS= 1
-const val SYSTEM= 2
+const val MARS = 1
+const val SYSTEM = 2
 
 class ViewPagerAdapter(private val fragmentManager: FragmentManager) :
     FragmentStatePagerAdapter(fragmentManager) {
@@ -19,12 +19,14 @@ class ViewPagerAdapter(private val fragmentManager: FragmentManager) :
     override fun getItem(position: Int) = fragments[position]
 
     override fun getPageTitle(position: Int): CharSequence? {
-        return when (position) {
+        return null
+/*        return when (position) {
             EARTH -> "Earth"
             MARS -> "Mars"
             SYSTEM -> "System"
             else ->"null"
         }
+*/
     }
 
 }
