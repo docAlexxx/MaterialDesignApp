c96f97babe4d246fbd0dbaca66b1bab800969469
docAlexxx <docalex@mail.ru>
2022-03-22 14:44:58 +0400

    lesson3: added ViewPagerAdapter

---
+++ app/src/main/java/com/example/materialdesignapp/view/viewpager/ViewPagerAdapter.kt
@@ -0,0 +1,18 @@
+package com.example.materialdesignapp.view.viewpager
+
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+
+
+class ViewPagerAdapter(private val fragmentManager: FragmentManager) :
+    FragmentStatePagerAdapter(fragmentManager) {
+
+    private val fragments = arrayOf(EarthFragment(), MarsFragment(), SystemFragment())
+
+    override fun getCount(): Int {
+        return fragments.size
+    }
+
+    override fun getItem(position: Int) = fragments[position]
+
+}
