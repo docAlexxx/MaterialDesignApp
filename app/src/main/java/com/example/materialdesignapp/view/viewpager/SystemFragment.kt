d57c178851715f318e9c42843d1edfd7e8c8dbd3
docAlexxx <docalex@mail.ru>
2022-03-28 23:08:03 +0400

    lesson5: ChangeImageTransform in SystemFragment

--- app/src/main/java/com/example/materialdesignapp/view/viewpager/SystemFragment.kt
+++ app/src/main/java/com/example/materialdesignapp/view/viewpager/SystemFragment.kt
@@ -1,20 +1,44 @@
 package com.example.materialdesignapp.view.viewpager
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import com.example.materialdesignapp.R
+import android.widget.ImageView
+import androidx.transition.ChangeBounds
+import androidx.transition.ChangeImageTransform
+import androidx.transition.TransitionManager
+import com.example.materialdesignapp.databinding.FragmentSystemBinding
+import com.example.materialdesignapp.utils.BindingFragment
 
 
-class SystemFragment : Fragment() {
+class SystemFragment : BindingFragment<FragmentSystemBinding>(FragmentSystemBinding::inflate) {
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_system, container, false)
+    private var isOnCenter = false
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.imageViewSystem.setOnClickListener {
+            isOnCenter = !isOnCenter
+
+            val changeBounds = ChangeBounds()
+            val changeImageTransform = ChangeImageTransform()
+            changeBounds.duration = 3000
+            changeImageTransform.duration = 3000
+            TransitionManager.beginDelayedTransition(
+                binding.transitionsContainer,
+                changeImageTransform
+            )
+            //   with(binding.imageViewSystem.scaleType) {
+            if (isOnCenter) {
+                binding.imageViewSystem.scaleType = ImageView.ScaleType.CENTER_CROP
+            } else {
+                binding.imageViewSystem.scaleType = ImageView.ScaleType.CENTER_INSIDE
+            }
+            //    }
+        }
+
     }
+
+    override val viewModel: Any
+        get() = TODO("Not yet implemented")
 }
