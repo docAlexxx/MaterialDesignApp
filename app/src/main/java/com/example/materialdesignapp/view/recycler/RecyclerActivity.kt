a68b6a49643cce2ee10369b960a3ab5dd261b523
docAlexxx <docalex@mail.ru>
2022-04-01 00:37:29 +0400

    lesson6: add description view

--- app/src/main/java/com/example/materialdesignapp/view/recycler/RecyclerActivity.kt
+++ app/src/main/java/com/example/materialdesignapp/view/recycler/RecyclerActivity.kt
@@ -8,26 +8,26 @@ import com.example.materialdesignapp.databinding.ActivityRecyclerBinding
 class RecyclerActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityRecyclerBinding
-    lateinit var adapter :RecyclerActivityAdapter
+    lateinit var adapter: RecyclerActivityAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRecyclerBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val data = arrayListOf(
-            Data("",type = TYPE_HEADER),
-            Data("Earth",type = TYPE_EARTH),
-            Data("Earth",type = TYPE_EARTH),
-            Data("Mars", "",type = TYPE_MARS),
-            Data("Earth",type = TYPE_EARTH),
-            Data("Earth",type = TYPE_EARTH),
-            Data("Earth",type = TYPE_EARTH),
-            Data("Mars", "",type = TYPE_MARS)
+            Pair(false, Data("", type = TYPE_HEADER)),
+            Pair(false, Data("Earth", type = TYPE_EARTH)),
+            Pair(false, Data("Earth", type = TYPE_EARTH)),
+            Pair(false, Data("Mars", "", type = TYPE_MARS)),
+            Pair(false, Data("Earth", type = TYPE_EARTH)),
+            Pair(false, Data("Earth", type = TYPE_EARTH)),
+            Pair(false, Data("Earth", type = TYPE_EARTH)),
+            Pair(false, Data("Mars", "", type = TYPE_MARS))
         )
 
 
         adapter = RecyclerActivityAdapter({
-            Toast.makeText(this@RecyclerActivity, it.someText, Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, it.someText, Toast.LENGTH_SHORT).show()
         }, data)
 
         binding.recyclerView.adapter = adapter
--- app/src/main/java/com/example/materialdesignapp/view/recycler/RecyclerActivityAdapter.kt
+++ app/src/main/java/com/example/materialdesignapp/view/recycler/RecyclerActivityAdapter.kt
@@ -10,11 +10,11 @@ import com.example.materialdesignapp.databinding.ActivityRecyclerItemMarsBinding
 
 class RecyclerActivityAdapter(
     private val onListItemClickListener: OnListItemClickListener,
-    private var dataItem: MutableList<Data>
+    private var dataItems: MutableList<Pair<Boolean, Data>>
 ) : RecyclerView.Adapter<RecyclerActivityAdapter.BaseViewHolder>() {
 
     override fun getItemViewType(position: Int): Int {
-        return dataItem[position].type
+        return dataItems[position].second.type
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder {
@@ -46,67 +46,79 @@ class RecyclerActivityAdapter(
     }
 
     override fun onBindViewHolder(holder: BaseViewHolder, position: Int) {
-        holder.bind(dataItem[position])
+        holder.bind(dataItems[position])
     }
 
     abstract class BaseViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        abstract fun bind(data: Data)
+        abstract fun bind(data: Pair<Boolean, Data>)
     }
 
     fun addItem() {
-        dataItem.add(generateNewItem())
+        dataItems.add(generateNewItem())
         notifyItemInserted(itemCount - 1)
     }
 
 
-    private fun generateNewItem() = Data("new Mars", type = TYPE_MARS)
+    private fun generateNewItem() = Pair(false, Data("new Mars", type = TYPE_MARS))
 
-    override fun getItemCount() = dataItem.size
+    override fun getItemCount() = dataItems.size
 
     inner class EarthViewHolder(view: View) : BaseViewHolder(view) {
-        override fun bind(data: Data) {
+        override fun bind(data: Pair<Boolean, Data>) {
             ActivityRecyclerItemEarthBinding.bind(itemView).apply {
                 earthImageView.setOnClickListener {
-                    onListItemClickListener.onItemClick(data)
+                    onListItemClickListener.onItemClick(data.second)
                 }
             }
         }
     }
 
-
     inner class MarsViewHolder(view: View) : BaseViewHolder(view) {
-        override fun bind(data: Data) {
+        override fun bind(data: Pair<Boolean, Data>) {
             ActivityRecyclerItemMarsBinding.bind(itemView).apply {
                 marsImageView.setOnClickListener {
-                    onListItemClickListener.onItemClick(data)
+                    onListItemClickListener.onItemClick(data.second)
                 }
                 addItemImageView.setOnClickListener { addItemByPosition() }
                 removeItemImageView.setOnClickListener { removeItem() }
-                moveItemUp.setOnClickListener { if (layoutPosition>1) moveUp() }
-                moveItemDown.setOnClickListener { if (layoutPosition<dataItem.size-1) moveDown() }
+                moveItemUp.setOnClickListener { if (layoutPosition > 1) moveUp() }
+                moveItemDown.setOnClickListener { if (layoutPosition < dataItems.size - 1) moveDown() }
+                marsTextView.setOnClickListener { showDescription() }
+                marsDescriptionTextView.visibility =
+                    if (data.first == false) View.GONE else View.VISIBLE
+
             }
         }
 
+        private fun showDescription() {
+            dataItems[layoutPosition] = dataItems[layoutPosition].let {
+                val currentState = !it.first
+                Pair(currentState, it.second)
+            }
+            notifyItemChanged(layoutPosition)
+
+        }
+
         private fun addItemByPosition() {
-            dataItem.add(layoutPosition + 1, generateNewItem())
+            dataItems.add(layoutPosition + 1, generateNewItem())
             notifyItemInserted(layoutPosition + 1)
         }
 
         private fun removeItem() {
-            dataItem.removeAt(layoutPosition)
+            dataItems.removeAt(layoutPosition)
             notifyItemRemoved(layoutPosition)
         }
 
         private fun moveUp() {
-            dataItem.removeAt(layoutPosition).apply {
-                dataItem.add(layoutPosition - 1, this)
+            dataItems.removeAt(layoutPosition).apply {
+                dataItems.add(layoutPosition - 1, this)
             }
             notifyItemMoved(layoutPosition, layoutPosition - 1)
         }
 
         private fun moveDown() {
-            dataItem.removeAt(layoutPosition).apply {
-                dataItem.add(layoutPosition + 1, this)
+            dataItems.removeAt(layoutPosition).apply {
+                dataItems.add(layoutPosition + 1, this)
             }
             notifyItemMoved(layoutPosition, layoutPosition + 1)
         }
@@ -114,10 +126,10 @@ class RecyclerActivityAdapter(
     }
 
     inner class HeaderViewHolder(view: View) : BaseViewHolder(view) {
-        override fun bind(data: Data) {
+        override fun bind(data: Pair<Boolean, Data>) {
             ActivityRecyclerItemHeaderBinding.bind(itemView).apply {
                 header.setOnClickListener {
-                    onListItemClickListener.onItemClick(data)
+                    onListItemClickListener.onItemClick(data.second)
                 }
             }
         }
