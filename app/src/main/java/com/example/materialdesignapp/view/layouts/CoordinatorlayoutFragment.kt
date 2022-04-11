922c0321f506ee332ea42b4a267072f47f20454e
docAlexxx <docalex@mail.ru>
2022-04-11 15:09:55 +0400

    lesson7: get font from com.google.android.gms.fonts

--- app/src/main/java/com/example/materialdesignapp/view/layouts/CoordinatorlayoutFragment.kt
+++ app/src/main/java/com/example/materialdesignapp/view/layouts/CoordinatorlayoutFragment.kt
@@ -1,7 +1,12 @@
 package com.example.materialdesignapp.view.layouts
 
+import android.graphics.Typeface
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
+import androidx.core.provider.FontRequest
+import androidx.core.provider.FontsContractCompat
+import com.example.materialdesignapp.R
 import com.example.materialdesignapp.databinding.FragmentCoordinatorlayoutBinding
 import com.example.materialdesignapp.utils.BindingFragment
 
@@ -11,7 +16,26 @@ class CoordinatorlayoutFragment : BindingFragment<FragmentCoordinatorlayoutBindi
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setFont()
+
     }
+
+    private fun setFont() {
+        val request = FontRequest("com.google.android.gms.fonts","com.google.android.gms","Lemonada",
+            R.array.com_google_android_gms_fonts_certs)
+
+        val callback = object : FontsContractCompat.FontRequestCallback(){
+            override fun onTypefaceRetrieved(typeface: Typeface?) {
+                super.onTypefaceRetrieved(typeface)
+                binding.coordTextView.typeface =typeface
+
+            }
+        }
+        FontsContractCompat.requestFont(requireContext(),request,callback,
+            Handler(requireActivity().mainLooper)
+        )
+    }
+
 
     override val viewModel: Any
         get() = TODO("Not yet implemented")
